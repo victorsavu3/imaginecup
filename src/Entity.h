b5c4b23263eb689240408c4b992ae20ed2796a7a
Victor Savu <victorsavu3@gmail.com>
2013-03-16 20:31:26 +0200

    let Entity be simply a sf::Sprite

--- src/Entity.h
+++ src/Entity.h
@@ -6,6 +6,9 @@
 #include "Updatable.h"
 
 class Entity : public Updatable, public sf::Sprite{
+public:
+	virtual ~Entity() {}
+	virtual void step(float frame, float time) {}
 };
 
 #endif /* ENTITY_H_ */
